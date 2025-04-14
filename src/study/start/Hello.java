8a821e9383270c77470364472a2d6f5a50555de5
oaihr <rhksgh0710@gmail.com>
2025-04-14 11:35:41 +0900

    Java 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,22 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!");
+		
+		// 실행 (Run) : Ctrl + F11
+		
+//		 주석 단축키:   Ctrl + /
+//	    			 Ctrl + Shift + c
+//	    			 Ctrl + Shift + /
+		
+		/* 
+		 * 여러줄 주석
+		 */
+		
+		
+	}
+
+}

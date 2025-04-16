6fdba4fa142481c2c139006665294779b86f6905
oaihr <rhksgh0710@gmail.com>
2025-04-16 15:08:20 +0900

    연습 5,6 입출력&연산자

---
+++ src/study/practice/Practice05.java
@@ -0,0 +1,45 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class Practice05 {
+
+	public static void main(String[] args) {
+
+		//1번. num 값을 확인하여 "양수" "음수" "0" 이라고 출력하도록 코드를 작성하세요.
+		
+//		int num = -19;
+//		String result = (num == 0) ? "0" : ((num >= 1 ) ? "양수" : "음수");
+//		System.out.println(result);
+//		
+		
+
+//		//2번. 아래와 같이 프로그램이 진행되면서 숫자 2개를 입력받고 각각의 결과를 출력하세요.
+		// ㅇ 모양은 사용자가 입력한 숫자와 계산된 결과입니다.
+		// 나누기 결과는 소수점을 포함하여 나타나도록 해주세요.
+//		첫번째 숫자를 입력하세요 : ㅇ
+//		입력한 숫자=ㅇ
+//		두번째 숫자를 입력하세요 : ㅇ
+//		입력한 숫자=ㅇ
+//
+//		더하기 : ㅇ+ㅇ=ㅇ
+//		뺴기 : ㅇ-ㅇ=ㅇ
+//		곱하기 : ㅇ*ㅇ=ㅇ
+//		나누기 : ㅇ/ㅇ=ㅇ
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("첫번째 숫자를 입력하세요 : ");		
+//		int inputData = scanner.nextInt();
+//		System.out.println("입력한 숫자="+ inputData);
+//		System.out.print("두번째 숫자를 입력하세요 : ");
+//		int inputData2 = scanner.nextInt();
+//		System.out.println("입력한 숫자="+ inputData2);
+//		
+//		System.out.println("더하기 : "+ (inputData+inputData2));
+//		System.out.println("빼기 : "+ (inputData-inputData2));
+//		System.out.println("곱하기 : "+ (inputData*inputData2));
+//		System.out.println("나누기 : "+ ((double)inputData/inputData2));
+		
+	}
+
+}
---
+++ src/study/practice/Practice06.java
@@ -0,0 +1,82 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class Practice06 {
+
+	public static void main(String[] args) {
+
+		// 1번. 삼각형의 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		// 삼각형의 넓이 = 밑변 * 높이 / 2 ;
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("삼각형 밑변의 길이는? : ");
+//		int base = scanner.nextInt();
+//		System.out.print("삼각형 높이는? : ");
+//		int height = scanner.nextInt();
+//		System.out.print("삼각형 넓이 : "+base * height / 2.0 );
+		
+		
+		// 2번. 사다리꼴의 윗변과 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		// 사다리꼴의 넓이 = (윗변 길이 + 밑변 길이) * 높이 / 2;
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("윗변의 길이는? : ");
+//		int upper = scanner.nextInt();
+//		System.out.print("밑변의 길이는? : ");
+//		int lower = scanner.nextInt();
+//		System.out.print("높이는? : ");
+//		int height = scanner.nextInt();
+//		System.out.print("사다리꼴 넓이 : "+ (upper + lower) * height / 2.0 );
+		
+		
+		// 3번. 우리반은 24명입니다. 과자가 100개 있습니다.
+		// 모두에게 공정하게 동일한 숫자의 과자를 나누어주면
+		// 한 명당 몇 개의 과자를 받게되는지와 나누어주고 나서 몇개의 과자가 남는지 출력하세요.
+		
+//		int perPerson = 100 / 24;
+//		int remain = 100 % 24;
+//		System.out.println("한 명당 받게 될 과자의 개수는: "+perPerson+ "개");
+//		System.out.println("나눠주고 남은 과자의 개수는: "+remain+ "개");
+		
+				
+		// 4번. 세자리의 정수를 입력 받아서 백의자리, 십의자리, 일의자리를 각각 출력하세요.
+		// 158
+		// 백의자리 : 1
+		// 십의자리 : 5
+		// 일의자리 : 8
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("세자리의 정수 입력: ");
+//		int number = scanner.nextInt();
+//		int hundred = number / 100;
+//		int ten = (number % 100 ) / 10;
+//		int one = number % 10;
+//		
+//		System.out.println("백의자리 : "+hundred);
+//		System.out.println("십의자리 : "+ten);
+//		System.out.println("일의자리 : "+one);
+		
+
+		// 5번 x와 y 변수 안에 들어있는 서로의 값을 바꾸기
+//		int x = 500;
+//		int y = 100;
+//		System.out.println("x="+x+ " y="+y);
+//		
+		//x와 y를 바꾼 작업1
+//		int z = x;
+//		x = y;
+//		y = z;
+		
+		//x와 y를 바꾼 작업2
+//		x = x^y;
+//		y = x^y;
+//		x = x^y;
+		
+//    	정수형 변수인 x,y값을 2진수 표현으로 바꾸고 각 비트마다 배타적 논리합을 이용해서 2진수 값을 구함 
+		
+//		System.out.println("x="+x+ " y="+y);
+
+	}
+
+}

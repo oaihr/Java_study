40d783677881efdfe571684d5e9cd8cfa5e81ea2
oaihr <rhksgh0710@gmail.com>
2025-04-16 11:19:19 +0900

    연습 3,4 변수& 타입변환&연산자

---
+++ src/study/practice/Practice03.java
@@ -0,0 +1,55 @@
+package study.practice;
+
+public class Practice03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		1. 아래 1), 2) 에 맞게 풀이.
+//		int x = 10;
+//		int y = 4;
+//		double result = (double)x/y;
+//		System.out.println(result);
+//	
+//		1) 결과가 2가 나오도록 작성하세요  int, (int)
+//		2) 결과가 2.5가 나오도록 작성하세요 double, (double)
+//		
+//		2. 결과가 7.5가 나오도록 작성하세요 
+//		double a = 3.5;
+//		double b = 4.7;
+//		double result = a + (int)b;;
+//		System.out.println(result);
+//	
+//		3. 계산결과가 12가 나오도록 작성하세요  
+//		String a = "3.4";
+//		String b = "4";
+//		int result = (int)Double.parseDouble(a) * Integer.parseInt(b);
+//		System.out.println(result);
+//	
+//	
+//		4. 계산결과가 "1013" 이 나오도록 코드를 작성하세요.
+//		String a = "10";
+//		int b = 3;
+//		double c = 4.5;
+//		String result = a + String.valueOf(3*b+(int)c);
+//		System.out.println(result);
+//	
+//	
+//		5. 계산결과가 "243" 이 나오도록 코드를 작성하세요.
+//		int a = 4;
+//		double b = 3.4;
+//		String c = "6.8";
+//		String result = String.valueOf((int)b*81);
+//		System.out.println(result);
+//	
+//		6. 계산 결과가 아래와 같이 나오도록 코드를 작성하세요.
+//		int x = 111;
+//		int y = 13;
+//		int result =   ;
+//		System.out.println(result);
+//	
+//		1) 출력결과 : 7	답: x - 8 * y
+//		2) 출력결과 : 8	답: x - 8 * y + 1
+	}
+
+}

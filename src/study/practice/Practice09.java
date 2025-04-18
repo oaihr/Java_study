4aff64e4b76a65e737a9f76ae79891d40697854b
oaihr <rhksgh0710@gmail.com>
2025-04-18 12:38:48 +0900

    연습9 반복문

---
+++ src/study/practice/Practice09.java
@@ -0,0 +1,72 @@
+package study.practice;
+
+public class Practice09 {
+
+	public static void main(String[] args) {
+		//반복문 샘플 연습
+		// 출력은 반복되는 숫자(i) 를 출력
+		
+		//for.
+		//1) i가 0~10으로 11번 반복
+//		for(int i=0; i<=10; i++){
+//			System.out.println(i);
+//		}
+		
+		//2) i가 1~9로 9번 반복
+//		for (int i=1; i<=9; i++) {
+//			System.out.println(i);
+//		}
+		
+		//3) i가 10 ~ 100 으로 10번 반복
+//		for (int i=10; i<=100; i+=10) {
+//			System.out.println(i);
+//		}
+		
+		//4) i가 25 50 75 100 해서 4번 반복
+//		for (int i=25; i<=100; i+=25) {
+//			System.out.println(i);
+//		}
+		
+		//while.
+		//5) i가 50 ~ 55로 6번 반복
+//		int i = 50;
+//		while(i<56) {
+//			System.out.println(i);
+//			i++;			
+//		}
+		
+		
+		//6) i가 10 ~ 1로 10번 반복
+//		int i = 10;
+//		while(i>0) {
+//			System.out.println(i);
+//			i--;
+//		}
+
+		
+		//7) i가 30 ~ 0으로 5씩 감소하면서 7번 반복
+//		int i = 30;
+//		while(i >= 0) {
+//			System.out.println(i);
+//			i -= 5;
+//		}
+		
+		//8) i가 30 ~ 5으로 5씩 감소하면서 6번 반복
+//		int i = 30;
+//		while(i >= 5) {
+//			System.out.println(i);
+//			i -= 5;
+//		}
+
+
+		//9) i가 25 ~ 0으로 5씩 감소하면서 6번 반복
+//		int i = 25;
+//		while(i >= 0) {
+//			System.out.println(i);
+//			i -= 5;
+//		}
+		
+
+	}
+
+}

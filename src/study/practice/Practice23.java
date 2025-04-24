66f877b362f89b831aabd08d95eee95fec13c18a
oaihr <rhksgh0710@gmail.com>
2025-04-24 10:06:11 +0900

    연습 23 종합연습 v2

--- src/study/practice/Practice23.java
+++ src/study/practice/Practice23.java
@@ -80,16 +80,60 @@ public class Practice23 {
 	}
 	
 	public static void prac2() {
-	
+		
+		//리팩토링 : 결과의 변경 없이 코드의 구조를 재조정
+		
 		String[] menuArr = {"아메리카노", "카페라떼", "바닐라라떼"};
 		int[] priceArr = {3500, 4100, 4300};		
 		int[] countArr = new int[3];  //0 초기화 
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("======== 메뉴 ========");
-		for(int i=0; i<=menuArr.length; i++ ){
-			System.out.printf("&d. %-6S      %d원", (i+1), menuArr[i], priceArr[i] );
+		while (true) {
+			System.out.println("======== 메뉴 ========");
+			for (int i = 0; i < menuArr.length; i++) {
+				System.out.printf("%d. %s \t%d원\n", (i + 1), menuArr[i], priceArr[i]);
+			}
+			System.out.println("=====================");
+			System.out.print("메뉴 선택 : ");
+			int menu = scanner.nextInt();
+			if (menu < 1 || menu > menuArr.length) {
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+				continue;
+			}
+			System.out.print("수량 선택 : ");
+			int count = scanner.nextInt();
+
+			countArr[menu-1] = countArr[menu-1] + count; // 모든 메뉴에 대해 수량 처리 코드가 공통 적용
+			
+			String ans;
+			while (true) {
+				System.out.print("추가 주문하시겠습니까?(y/n): ");
+				ans = scanner.next();
+				if (!( ans.equals("y") || ans.equals("n") ) ) {
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");					
+				} else {
+					break;
+				}
+			}					
+									
+			if (ans.equals("y")) {
+				continue;
+			} else if (ans.equals("n")) {				
+				break;
+			}		
+				
+		}	
+		
+		int total = 0;		
+
+		System.out.println("====================");
+		for (int i = 0; i < menuArr.length; i++) {
+			if (countArr[i] > 0) {
+				System.out.printf("%s %d잔 : %d원\n", menuArr[i], countArr[i], (priceArr[i] * countArr[i]));
+				total = total + (priceArr[i] * countArr[i]);  // 1잔 이상 시킨 메뉴만 총합에 들어가므로 같이 for문 돌림
+			}
 		}
 		System.out.println("====================");
+		System.out.printf("총액 : %d원", total);
 	}
-
 }

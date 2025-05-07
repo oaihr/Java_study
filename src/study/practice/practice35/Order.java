038af7c711ca42eab89c12aacdfcd55e8973c522
oaihr <rhksgh0710@gmail.com>
2025-05-07 16:00:28 +0900

    연습35+ ArrayList 변환

--- src/study/practice/practice35/Order.java
+++ src/study/practice/practice35/Order.java
@@ -1,22 +1,24 @@
 package study.practice.practice35;
 
+import java.util.ArrayList;
+
 public class Order {
 	// 필드(인스턴스 필드)
 		int orderNum;
-		Menu[] menus;
-
+//		Menu[] menus;
+		ArrayList<Menu> menus;
 	// 생성자
-		public Order(int i, Menu[] arr) {
+		public Order(int i, ArrayList<Menu> arrList) {
 			orderNum = i;
-			menus = arr;
+			menus = arrList;
 		}
 
 	// 메소드(인스턴스 메소드)
 		public int totalPrice() {
 			/* 1. 모든 주문 메뉴의 총합을 반환하세요. */
 			int total = 0;
-			for(int i=0; i<menus.length; i++) {
-				total = total + menus[i].price;
+			for(int i=0; i<menus.size(); i++) {
+				total = total + menus.get(i).price;
 			}
 			return total;
 		}

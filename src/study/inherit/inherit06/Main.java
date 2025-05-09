a605d95c7d71a6975b47a45386c5f235b4d40100
oaihr <rhksgh0710@gmail.com>
2025-05-09 14:30:43 +0900

    클래스 상속 강제 타입 변환

--- src/study/inherit/inherit06/Main.java
+++ src/study/inherit/inherit06/Main.java
@@ -24,6 +24,40 @@ public class Main {
 		//Bus b4 = takeVehicle();	// takeVehicle()의 리턴 타입이 Vehicle이라 안됨
 		// Bus   = Vehicle
 		Vehicle v4 = takeVehicle();
+		
+//		===========================================================================
+		
+		Vehicle vv = new Vehicle();
+		
+		Vehicle vv2 = new Bus(); //자동 타입 변환
+		//vv2.run();    O
+		//vv2.start();  X
+		Bus bb = new Bus();
+		bb.run();
+		bb.start();
+		
+		Bus bb2 = (Bus)vv2;
+		bb2.start();
+		
+		
+//		Bus bb3 = (Bus)vv;
+//		bb3.start();	// 안됨
+		
+//		===========================================================================
+		Vehicle vv4 = takeVehicle();
+		
+		if(vv4 instanceof Bus) {
+			Bus bbb = (Bus)vv4;
+			System.out.println("Bus가 들어있음");
+		}
+		if(vv4 instanceof Taxi) {
+			Taxi ttt = (Taxi)vv4;
+			System.out.println("Taxi가 들어있음");
+		}
+		//Bus bbb = (Bus)vv4;
+		//bbb.start();
+		
+		
 	}
 
 	public static Bus takeBus() {		
@@ -38,8 +72,15 @@ public class Main {
 	
 	public static Vehicle takeVehicle() {		
 //		return new Vehicle();
-		return new Bus();
+//		return new Bus();
 //		return new Taxi();
+		
+		int n = (int)(Math.random()*2) + 1;
+		if(n == 1) 
+			return new Bus();
+		else
+			return new Taxi();
+		
 	}
 	
 	

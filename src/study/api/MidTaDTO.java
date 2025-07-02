b361b60aff6c8676803d4d4a90f63ef20297c9e7
oaihr <rhksgh0710@gmail.com>
2025-07-02 16:43:43 +0900

    공공데이터 포털 API 활용하기1

---
+++ src/study/api/MidTaDTO.java
@@ -0,0 +1,53 @@
+package study.api;
+
+public class MidTaDTO {
+	
+	String resultCode;
+	String resultMsg;
+	
+	String redId;
+	int taMin4;
+	int taMax4;
+	int taMin5;
+	int taMax5;
+	
+	public String getResultCode() {
+		return resultCode;
+	}
+	public void setResultCode(String resultCode) {
+		this.resultCode = resultCode;
+	}
+	public String getResultMsg() {
+		return resultMsg;
+	}
+	public void setResultMsg(String resultMsg) {
+		this.resultMsg = resultMsg;
+	}
+	public String getRedId() {
+		return redId;
+	}
+	public void setRedId(String redId) {
+		this.redId = redId;
+	}
+	public int getTaMin4() {
+		return taMin4;
+	}
+	public void setTaMin4(int taMin4) {
+		this.taMin4 = taMin4;
+	}
+	public int getTaMax4() {
+		return taMax4;
+	}
+	public void setTaMax4(int taMax4) {
+		this.taMax4 = taMax4;
+	}
+	public int getTaMax5() {
+		return taMax5;
+	}
+	public void setTaMax5(int taMax5) {
+		this.taMax5 = taMax5;
+	}
+	
+	
+	
+}
